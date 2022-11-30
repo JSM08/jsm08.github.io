a173d19b6bf69c6ed3a35a1600c3a83333b8ff7a
JSM08 <115659336+JSM08@users.noreply.github.com>
2022-11-30 11:22:59 -0500

    hhh

---
+++ practice/js/practice.js
@@ -0,0 +1,15 @@
+console.log("how are you")
+
+alert("goodbye")
+
+
+let weather = "Partly cloudy";
+
+let myDate = 28;
+
+let temperature;
+
+myDate = myDate + 1;
+temperature = 81;
+
+console.log("November " + myDate + " Weather: " + weather + " at " + temperature +"degrees Fahrenheit ");
