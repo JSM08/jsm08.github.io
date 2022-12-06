5ec1e178db9b79326caa08f2727f278b49cff8e4
JSM08 <115659336+JSM08@users.noreply.github.com>
2022-12-06 13:55:36 -0500

    new

--- lightbox/js/lightbox.js
+++ lightbox/js/lightbox.js
@@ -16,9 +16,59 @@ function openLightbox(){
 
 }
 
-let coffeeImage = document.getElementById('coffeeImage1');
+let photo = document.getElementById('photo4');
 //when you click image, refernce openlightbox function
-coffeeImage.onclick = openLightbox;
+photo.onclick = openLightbox;
+
+//make function to make closer clickable
+
+function closeLightbox(){
+    lightboxContainer.classList.remove('display');
+    lightbox.src ="";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+
+
+
+let hands = document.getElementById('hands2');
+//when you click image, refernce openlightbox function
+hands.onclick = openLightbox;
+
+//make function to make closer clickable
+
+function closeLightbox(){
+    lightboxContainer.classList.remove('display');
+    lightbox.src ="";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+
+
+let art = document.getElementById('art3');
+//when you click image, refernce openlightbox function
+art.onclick = openLightbox;
+
+//make function to make closer clickable
+
+function closeLightbox(){
+    lightboxContainer.classList.remove('display');
+    lightbox.src ="";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+
+
+
+let me = document.getElementById('me4');
+//when you click image, refernce openlightbox function
+me.onclick = openLightbox;
 
 //make function to make closer clickable
 
