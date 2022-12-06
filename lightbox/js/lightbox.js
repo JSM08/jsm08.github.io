be3e8e6809f87d5622ce406fa41a1a516fca2e4d
JSM08 <115659336+JSM08@users.noreply.github.com>
2022-12-06 12:56:03 -0500

    coffeeeeee

---
+++ lightbox/js/lightbox.js
@@ -0,0 +1,31 @@
+let lightboxContainer = document.getElementById('lightboxContainer');
+
+let lightboxBackground = document.getElementById('lightboxBackground');
+
+let lightbox = document.getElementById('lightbox');
+
+let lightboxCloser = document.getElementById('lightboxCloser');
+
+// function to open lightbox
+function openLightbox(){
+    //turn on dispaly class (from display : none to dispaly: flex)
+    lightboxContainer.classList.add
+ ('display');
+ //dispaly image src inside lightbox
+ lightbox.src = this.src;
+
+}
+
+let coffeeImage = document.getElementById('coffeeImage1');
+//when you click image, refernce openlightbox function
+coffeeImage.onclick = openLightbox;
+
+//make function to make closer clickable
+
+function closeLightbox(){
+    lightboxContainer.classList.remove('display');
+    lightbox.src ="";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
---
+++ praccctice/js/praccctice.js
@@ -0,0 +1,12 @@
+// choose the larger number
+function chooseLargerNumber(num1, num2){
+let larger;
+
+
+if (num1 >= num2) {
+larger = num1;
+} else { 
+    larger = num2;
+}
+return larger;
+}
