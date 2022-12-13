25f03fc479a44bca73decf1cf463e438fa6ee567
JSM08 <115659336+JSM08@users.noreply.github.com>
2022-12-13 13:53:53 -0500

    monster

--- newproject/js/newproject.js
+++ newproject/js/newproject.js
@@ -1,5 +1,12 @@
-//get container
+//variables
+
 let animationMonster = document.getElementById("animationMonster");
+
+let animationToggle = document.getElementById("animationToggle");
+
+let toggle_c = 0;
+
+
 
 let monster =
 bodymovin.loadAnimation({
@@ -17,3 +24,42 @@ bodymovin.loadAnimation({
     animationMonster.addEventListener('click' , function () {
       monster.playSegments([110, 215], true);
     })
+
+    let toggle =
+    bodymovin.loadAnimation({
+          container: animationToggle,
+          renderer: 'svg',
+          loop: false,
+          autoplay: false,
+          path: "https://lottie.host/21bd1bb8-726f-4e85-aab1-b7136b561215/ovslk7UxsA.json"
+        });
+    
+
+
+       
+
+        //toggle to one side
+  
+        animationToggle.addEventListener('click', function(){
+          if (toggle_c == 0) {
+            toggle.playSegments([1,20], true);
+            toggle_c = toggle_c + 1;
+          } else {
+            toggle.playSegments([21,60], true);
+            let toggle_c = 0 ;
+          }
+        }
+        );
+        
+
+
+        animationMonster.addEventListener (' mouseleave', function ()
+        {
+          monster.playSegments ([99,211], false);
+        });
+
+        animationMonster.addEventListener('mouseenter', function (){
+          monster.playSegments([256, 302], false);
+        });
+
+       
